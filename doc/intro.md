2c0fdbcba358d14c9dcdae304cfc00c9bc11c6c2
Anh Nguyet Vu <anngvu@gmail.com>
2023-05-30 21:34:36 -0600

    Set up as project, fix namespaces later

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to retold
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/retold/core_test.clj
@@ -0,0 +1,7 @@
+(ns retold.core-test
+  (:require [clojure.test :refer :all]
+            [retold.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
