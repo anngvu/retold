c9faf07b9b7036187e7ae4c34e762bf2fc8897b3
Anh Nguyet Vu <anngvu@gmail.com>
2023-06-01 08:23:09 -0600

    Update as_jsonld

--- src/retold/as_jsonld.clj
+++ src/retold/as_jsonld.clj
@@ -1,5 +1,6 @@
 (ns retold.as-jsonld
-  (:require [clj-yaml.core :as yaml]
+  (:require [clojure.java.io :as io]
+            [clj-yaml.core :as yaml]
             [babashka.cli :as cli]))
 
 (def cli-options {:file {}})
@@ -14,7 +15,7 @@
 (defn read-yaml [file]
   (yaml/parse-string (slurp file)))
 
-(def dm (read-yaml (opts :file)))
+;(def dm (read-yaml (opts :file)))
 
 (def slots (filter #(:slots %) dm))
 (def classes (filter #(:class %) dm))
@@ -27,30 +28,42 @@
     :schema "http://schema.org"
     :xsd "http://w3.org/2001/XMLSchema#"}})
 
-(defn translate-required [m]
-  (if (m :required)
+(defn sms-required [value m]
+  (if (value)
     (assoc m "sms:required" "sms:true")
     (assoc m "sms:required" "sms:false")))
+
+(defn sms-range [value m]
+  (assoc m "sms:rangeIncludes" :range value))
+
+; this might do fancier things in the future
+(defn sms-validation-rules [value m]
+  (assoc m "sms:validationRules" value))
 
 (defn which-type [t]
   (if (= "slots" t) "rdfs:Class" "rdf:Property"))
 
-(defn as-entity [m]
-  {"@id" (str ns1 id)
-   "@type" (str "rdfs:Class")
-   "rdfs:comment" (m :description)
-   "rdfs:label" (str ns1 id)
-   "rdfs:subClassOf" (into [] (m :subClassOf))
-   "schema:isPartOf" {"@id" schema-uri}
-   "sms:displayName" (str ns1 id)
-   "sms:required" (get m "sms:required")
-   "sms:validationRules" (get-in m [:annotations :validationRules] [])
-   ;"sms:rangeIncludes" (get-in enums [(get m :range) :permissible_values])
-   })
+(defn get-enum [range]
+  (get-in enums [(keyword range) :permissible_values]))
+
+(defn as-entity [m type]
+  (cond->
+      {"@id" (str ns1 id)
+       "@type" (str "rdfs:Class")
+       "rdfs:comment" (m :description)
+       "rdfs:label" (str ns1 id)
+       "rdfs:subClassOf" (into [] (m :subClassOf))
+       "schema:isPartOf" {"@id" schema-uri}
+       "sms:displayName" (str ns1 id)}
+       ((= "classes" type) (sms-required (get-in m [:annotations :required])))
+       ((= "enums" type) (sms-required (get m :required)))
+       ((= "slots" type) (sms-range (get-enum (get m :range))))
+       ((= "slots" type) (sms-validation-rules (get-in m [:annotations :validationRules]))))
+   )
 
 (defn make-graph [m]
   {"@id" "http://schema.biothings.io/#0.1"
    "@graph" (map #(as-entity %) m)})
 
-(process-file! (opts :file))
+;(process-file! (opts :file))
 ;(process-dir! (opts :dir))
