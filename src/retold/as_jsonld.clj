d41ff6e844aaea94e9a817ad6e7e51a33e3b62c4
Anh Nguyet Vu <anngvu@gmail.com>
2023-06-01 20:01:57 -0600

    Update src and deps

--- src/retold/as_jsonld.clj
+++ src/retold/as_jsonld.clj
@@ -1,10 +1,12 @@
 (ns retold.as-jsonld
   (:require [clojure.java.io :as io]
             [clj-yaml.core :as yaml]
+            [clojure.string :as str]
+            [jsonista.core :as json]
             [babashka.cli :as cli]))
 
 (def cli-options {:file {}})
-(def ns1 "bts:")
+(def default-ns "bts:")
 (def bts "http://schema.biothings.io")
 
 (def opts (cli/parse-opts *command-line-args* {:spec cli-options}))
@@ -15,11 +17,22 @@
 (defn read-yaml [file]
   (yaml/parse-string (slurp file)))
 
-;(def dm (read-yaml (opts :file)))
+(defn list-files [dir]
+  (map str (filter #(.isFile %) (file-seq (io/file dir)))))
 
-(def slots (filter #(:slots %) dm))
-(def classes (filter #(:class %) dm))
-(def enums (filter #(:enums %) dm))
+(defn files-into-map [files] (mapv read-yaml files))
+
+(defn filter-type [k]
+  (let [coll (filter #(k %) dm)]
+    (if (= 1 (count coll))
+      ((first coll) k)
+      (reduce #(merge (%1 k) (%2 k)) coll))))
+
+(def dm (files-into-map (list-files "data")))
+(def slots (filter-type :slots))
+(def classes (filter-type :classes))
+(def enums (filter-type :enums))
+
 (def default-context
   {"@context"
    {:bts bts
@@ -28,42 +41,47 @@
     :schema "http://schema.org"
     :xsd "http://w3.org/2001/XMLSchema#"}})
 
-(defn sms-required [value m]
-  (if (value)
-    (assoc m "sms:required" "sms:true")
-    (assoc m "sms:required" "sms:false")))
+(defn sms-required [derived m]
+  (if (get m :required)
+    (assoc derived "sms:required" "sms:true")
+    (assoc derived "sms:required" "sms:false")))
 
-(defn sms-range [value m]
-  (assoc m "sms:rangeIncludes" :range value))
+(defn sms-range [derived m]
+  (if (get m :enum_range)
+    (assoc derived "sms:rangeIncludes" (get m :enum_range))
+    (assoc derived "sms:rangeIncludes" (get-enum (get m :range)))))
 
 ; this might do fancier things in the future
-(defn sms-validation-rules [value m]
-  (assoc m "sms:validationRules" value))
-
-(defn which-type [t]
-  (if (= "slots" t) "rdfs:Class" "rdf:Property"))
+(defn sms-validation-rules [derived m]
+  (assoc derived "sms:validationRules" (get-in m [:annotations :validationRules])))
 
 (defn get-enum [range]
   (get-in enums [(keyword range) :permissible_values]))
 
-(defn as-entity [m type]
-  (cond->
-      {"@id" (str ns1 id)
-       "@type" (str "rdfs:Class")
+(defn as-entity [entity type]
+  (let [label (name (entity 0)) m (val entity)]
+    (cond->
+      {"@id" (str default-ns (make-class-id label))
+       "@type" (if (= "slots" type) "rdf:Property" "rdfs:Class")
        "rdfs:comment" (m :description)
-       "rdfs:label" (str ns1 id)
+       "rdfs:label" label
        "rdfs:subClassOf" (into [] (m :subClassOf))
-       "schema:isPartOf" {"@id" schema-uri}
-       "sms:displayName" (str ns1 id)}
-       ((= "classes" type) (sms-required (get-in m [:annotations :required])))
-       ((= "enums" type) (sms-required (get m :required)))
-       ((= "slots" type) (sms-range (get-enum (get m :range))))
-       ((= "slots" type) (sms-validation-rules (get-in m [:annotations :validationRules]))))
-   )
+       "schema:isPartOf" {"@id" bts}
+       "sms:displayName" label}
+       (= "classes" type) (sms-required m)
+       (= "enums" type) (sms-required m)
+       (= "slots" type) (sms-range m)
+       (= "slots" type) (sms-validation-rules m)
+       )))
 
-(defn make-graph [m]
-  {"@id" "http://schema.biothings.io/#0.1"
-   "@graph" (map #(as-entity %) m)})
+(def enum-graph (map #(as-entity % "enums") enums))
+(def prop-graph (map #(as-entity % "slots") slots))
+(def class-graph (map #(as-entity % "classes") classes))
 
-;(process-file! (opts :file))
-;(process-dir! (opts :dir))
+(def graph
+  {"@id" bts
+   "@graph" (merge enum-graph prop-graph class-graph)})
+
+(def json-file (java.io.File. "model.jsonld"))
+
+(json/write-value json-file graph)
