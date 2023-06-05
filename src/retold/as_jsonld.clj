fe0b64567dd77a640aeae8df0065cfbff2b4449c
Anh Nguyet Vu <anngvu@gmail.com>
2023-06-04 22:57:30 -0600

    Work around schematic bug

--- src/retold/as_jsonld.clj
+++ src/retold/as_jsonld.clj
@@ -78,7 +78,7 @@
   (let [label (name (entity 0)) m (val entity)]
     (cond->
       {"@id" (make-id label)
-       "@type" (if (= "slots" type) "rdf:Property" "rdfs:Class")
+       "@type" (if (= "slots" type) "linkml:SlotDefinition" "rdfs:Class")
        "rdfs:comment" (get m :description "TBD")
        "rdfs:label" label
        "schema:isPartOf" {"@id" bts}
