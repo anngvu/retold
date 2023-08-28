69b464ed4a74ab84208f06b6467ee1b3f5c31383
Anh Nguyet Vu <anngvu@gmail.com>
2023-08-27 20:15:00 -0600

    Revise

--- src/retold/as_linkml.clj
+++ src/retold/as_linkml.clj
@@ -29,37 +29,14 @@
    {:is_a (first (map new-id (:rdfs:subClassOf idx)))
     :slots (map new-id (:sms:requiresDependency idx)) }})
 
-(defn map-enum [x]
-  {:id (keyword (str (new-id x) "Enum"))
-   :values (map new-id (:schema:rangeIncludes x)) })
-
-(def enums
-  (->>(filter #(> (count (:schema:rangeIncludes %)) 1) g)
-      (map map-enum)))
-
-; Dedup enums
-(def enum-sets
-  (->>(cset/index (set enums) [:values])
-      (map (fn [[vals ids]] [vals {:id (map :id ids)}]))))
-
-; Check re-used enums
-;(filter #(> (count (second %)) 5) enum-sets)
-
-(defn format-enum [idx]
-  (let [[vset instances] idx]
-    {(first (:id instances))
-     {:permissible_values
-      (into {} (map (fn [v] {(keyword v) ""})(:values def)))}}))
-
-(def enum-export {:enums (into {} (map #(format-enum %) enum-sets)) })
-
-; Only export props actually used
-(def used
+(def used-props
   (->>(mapcat :sms:requiresDependency classes)
       (map :id)
       (set)))
 
-(def props (filter #(contains? used (:id %)) g))
+(def props
+  "Only export props actually used"
+  (filter #(contains? used-props (:id %)) g))
 
 (defn format-prop [idx]
   {(keyword (:rdfs:label idx))
@@ -70,7 +47,51 @@
     (idx :sms:validationRules) (assoc :annotations {:validationRules (:sms:validationRules idx) })
     (idx :schema:rangeIncludes) (assoc :range (str (:rdfs:label idx) "Enum"))) })
 
+(defn map-enum [x]
+  {:id (keyword (str (new-id x) "Enum"))
+   :title (:sms:displayName x)
+   :values (map :id (:schema:rangeIncludes x))
+   :prop (:id x)
+   :domain (:id (first (:rdfs:subClassOf x)))
+   })
+
+(defn eyanum-stats
+  "Show enums reused more than n times"
+  [n]
+  (filter #(> (count (:id (second %))) n) enum-sets))
+
+(defn in? [coll x] (some #(= x %) coll))
+  "Look for enums that are used"
+
+(def enums
+  (->>(filter #(> (count (:schema:rangeIncludes %)) 1) g)
+      (map map-enum)
+      (filter #(contains? used-props (% :prop)))))
+
+(defn id-to-display [coll]
+  (->>(filter #(in? coll (:id %)) g)
+      (map :sms:displayName)))
+
+(def enum-sets
+  (->>(cset/index (set enums) [:values])
+      (map (fn [[vals xs]] [vals {:id (map :id xs) :domain (first (map :domain xs))}]))))
+
+(defn empty-key [x] {(keyword x) nil})
+
+(defn format-enum [[vset instances]]
+  {(first (:id instances))
+   {:permissible_values
+    (into {} (map empty-key (id-to-display (:values vset)))) }})
+
+(defn enum-export [subset]
+  (if subset
+    {:enums (into {} (map #(format-enum %) (filter #(= subset (:domain (second %))) enum-sets)))}
+    {:enums (into {} (map #(format-enum %) enum-sets))}))
+
 (def prop-export {:slots (into {} (map #(format-prop %) props)) })
 
 (defn write-yaml! [export filename]
   (spit filename (yaml/generate-string export :dumper-options {:flow-style :block})))
+
+(doseq [e (set (map :domain enums))]
+  (write-yaml! (enum-export e) (str "enums/" (str/replace e "bts:" "") ".yaml")))
